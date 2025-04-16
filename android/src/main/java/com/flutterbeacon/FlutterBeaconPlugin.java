765d046f5a355a2c56a234b1bb1a05a4d4c607df
oleh.rostovtsev <oleh.rostovtsev@africafarmersnetwork.com>
2025-04-16 12:10:10 +0300

    test changes-1

--- android/src/main/java/com/flutterbeacon/FlutterBeaconPlugin.java
+++ android/src/main/java/com/flutterbeacon/FlutterBeaconPlugin.java
@@ -20,7 +20,6 @@ import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 public class FlutterBeaconPlugin implements FlutterPlugin, ActivityAware, MethodCallHandler,
     PluginRegistry.RequestPermissionsResultListener,
